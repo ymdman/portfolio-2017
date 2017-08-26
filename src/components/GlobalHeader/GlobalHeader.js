23ee60c680326b01661c492801240ed292b17f59
ymdman <revitalized@icloud.com>
2017-08-26 15:00:11 +0900

    :zap: ヘッダー用CSS追加

--- src/components/GlobalHeader/GlobalHeader.js
+++ src/components/GlobalHeader/GlobalHeader.js
@@ -1,7 +1,16 @@
 import React from 'react';
 
 const GlobalHeader = () => (
-  <header>header</header>
+  <header className="l-global-header">
+    <h1>Site Logo</h1>
+    <nav>
+      <ul>
+        <li>About</li>
+        <li>Skill</li>
+        <li>Career</li>
+      </ul>
+    </nav>
+  </header>
 );
 
 export default GlobalHeader;
