e170b9293604afb6061dd6adb9e0051596561daf
ymdman <revitalized@icloud.com>
2017-08-29 22:58:27 +0900

    :sparkles: pages追加

---
+++ src/pages/App/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GlobalHeader from '../../components/GlobalHeader/GlobalHeader';
+import GlobalFooter from '../../components/GlobalFooter/GlobalFooter';
+
+const App = () => (
+  <div className="wrapper">
+    <GlobalHeader />
+    <mian>main</mian>
+    <GlobalFooter />
+  </div>
+);
+
+export default App;
---
+++ src/pages/Career/Career.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Career = () => (
+  <p>Career</p>
+);
+
+export default Career;
