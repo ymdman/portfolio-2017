be65a2783a28459f845cde9961b8f5f61e510198
ymdman <revitalized@icloud.com>
2017-08-30 21:43:23 +0900

    :sparkles: ルーターの追加

--- src/components/GlobalHeader/GlobalHeader.js
+++ src/components/GlobalHeader/GlobalHeader.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 
 const GlobalHeader = () => (
   <header className="l-global-header">
     <h1>Site Logo</h1>
     <nav>
       <ul>
-        <li>About</li>
-        <li>Skill</li>
-        <li>Career</li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/skill">Skill</Link></li>
+        <li><Link to="/career">Career</Link></li>
       </ul>
     </nav>
   </header>
--- src/main.js
+++ src/main.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import App from './components/App';
+import { BrowserRouter, Route } from 'react-router-dom';
+import App from './pages/App/App';
+import About from './pages/About/About';
+import Skill from './pages/Skill/Skill';
+import Career from './pages/Career/Career';
 import './main.css';
 
 ReactDom.render(
-  <App />,
+  <BrowserRouter>
+    <Route component={App}>
+      <Route path="/" component={About} />
+      <Route path="/skill" component={Skill} />
+      <Route path="/career" component={Career} />
+    </Route>
+  </BrowserRouter>,
   document.getElementById('app'),
 );
