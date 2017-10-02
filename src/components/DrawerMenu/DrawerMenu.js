8f1050a3407d641c2987ba977f9ad1a498d4ef4c
ymdman <revitalized@icloud.com>
2017-10-02 21:27:54 +0900

    :art: ドロワーのリンク変更

--- src/components/DrawerMenu/DrawerMenu.js
+++ src/components/DrawerMenu/DrawerMenu.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-// import { connect } from 'react-redux';
+import { connect } from 'react-redux';
 
-const DrawerMenu = () => (
+const DrawerMenu = ({ menuClick }) => (
   <nav className="drawer-menu">
     <ul>
       <li><Link to="/">About</Link></li>
-      <li>Skill</li>
-      <li>Career</li>
+      <li><Link to="/" onClick={() => { menuClick(); }}>Skill</Link></li>
+      <li><Link to="/career">Career</Link></li>
     </ul>
   </nav>
 );
@@ -20,17 +20,36 @@ const DrawerMenu = () => (
 //   drawerMenu: React.PropTypes.bool.isRequired,
 // };
 
-// const mapStateToProps = (state) => {
-//   const data = {
-//     drawerMenu: state.ChangeDrawerMenu.drawerMenu,
-//   };
+DrawerMenu.propTypes = {
+  menuClick: React.PropTypes.func.isRequired,
+};
 
-//   return data;
-// };
 
-// export default connect(
-//   mapStateToProps,
-// )(DrawerMenu);
+const mapStateToProps = (state) => {
+  const data = {
+    drawerMenu: state.ChangeDrawerMenu.drawerMenu,
+  };
 
-export default DrawerMenu;
+  return data;
+};
+
+const mapDispatchToProps = (dispatch) => {
+  const data = {
+    menuClick: () => {
+      dispatch({
+        type: 'CHANGE_DRAWER_MENU',
+      });
+    },
+  };
+
+  return data;
+};
+
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(DrawerMenu);
+
+// export default DrawerMenu;
 
