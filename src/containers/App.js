c9b86114fdd60b06c3d9fce2d8ab7929d55e7115
ymdman <revitalized@icloud.com>
2017-10-11 23:00:43 +0900

    :art: Content作成

--- src/containers/App.js
+++ src/containers/App.js
@@ -7,6 +7,9 @@ import ActionCreator from '../actions/ActionCreator';
 import GlobalHeader from '../components/GlobalHeader/GlobalHeader';
 import GlobalFooter from '../components/GlobalFooter/GlobalFooter';
 import DrawerMenu from '../components/DrawerMenu/DrawerMenu';
+import ContentAbout from '../components/ContentAbout/ContentAbout';
+import ContentSkill from '../components/ContentSkill/ContentSkill';
+import ContentCareer from '../components/ContentCareer/ContentCareer';
 
 class App extends React.Component {
   constructor(props) {
@@ -22,16 +25,26 @@ class App extends React.Component {
   render() {
     const props = this.props;
 
+    let Content = <ContentAbout />;
+
+    if (props.currentState.contentType === 'about') {
+      Content = <ContentAbout />;
+    } else if (props.currentState.contentType === 'skill') {
+      Content = <ContentSkill />;
+    } else if (props.currentState.contentType === 'career') {
+      Content = <ContentCareer />;
+    }
+
     return (
       <div className="wrapper">
         <Sidebar
           sidebar={<DrawerMenu {...props} />}
-          open={this.props.currentState.drawerMenu}
+          open={props.currentState.drawerMenu}
           onSetOpen={this.onSetSidebarOpen}
         >
           <GlobalHeader {...props} />
           <main>
-            <p>lorem</p>
+            {Content}
           </main>
           <GlobalFooter />
         </Sidebar>
