9370593fb0d93edbc23c9d7b5882f952a215b768
ymdman <revitalized@icloud.com>
2017-10-24 22:13:13 +0900

    :art: aboutの内容追加

--- src/components/ContentAbout/ContentAbout.js
+++ src/components/ContentAbout/ContentAbout.js
@@ -4,7 +4,22 @@ import MainVisual from '../MainVisual/MainVisual';
 const ContentAbout = props => (
   <div>
     <MainVisual {...props} />
-    <p>About</p>
+    <div className="content">
+      <h2 className="title">About</h2>
+      <p>
+        フロントエンド・エンジニアKazuhiro Yamadaのポートフォリオサイトになります。<br />
+        当サイトはReact+Reduxを使用しSPAで実装しております。<br />
+        ソースはGitHubにて公開してますのでご確認頂けると幸いです。
+      </p>
+      <ul className="list">
+        <li>React</li>
+        <li>Redux</li>
+        <li>ES2015</li>
+        <li>webpack 3</li>
+        <li>PostCSS</li>
+        <li>cssnext</li>
+      </ul>
+    </div>
   </div>
 );
 
