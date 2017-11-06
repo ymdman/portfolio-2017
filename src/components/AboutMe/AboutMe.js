36dc4cf32bd5dab7e3d65b15da42836a589bc0fc
ymdman <revitalized@icloud.com>
2017-11-06 22:06:21 +0900

    :art: AboutMeテキスト変更

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -2,12 +2,13 @@ import React from 'react';
 
 const AboutMe = () => (
   <div className="about-me">
-    <div className="about-me__image"><img src="https://dummyimage.com/300x300/000/fff" alt="" /></div>
+    <div className="about-me__image">
+      <img src="/images/img-kazuhiro-yamada.jpg" alt="Kazuhiro Yamadaの写真" />
+    </div>
     <div className="about-me__content">
-      <p>Kazuhiro Yamada</p>
-      <p>ポートフォリオサイトになりますポートフォリオサイト<br />
-      になりますポートフォリオサイトになります<br />
-      ポートフォリオサイトになりますポートフォリオサイトになります</p>
+      <h3 className="about-me-title">Kazuhiro Yamada</h3>
+      <h4 className="about-me-sub-title">Front-End Engineer / Web-Designer</h4>
+      <p className="about-me-text">JavaScriptが好きです。JavaScriptが好きです。</p>
     </div>
   </div>
 );
