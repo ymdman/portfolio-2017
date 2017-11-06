7d930d65de1f62b30db862ab9aa54f81152230ea
ymdman <revitalized@icloud.com>
2017-11-06 22:57:37 +0900

    :art: react-octicons追加

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { MarkGithubIcon } from 'react-octicons';
 
 const AboutMe = () => (
   <div className="about-me">
@@ -9,6 +10,9 @@ const AboutMe = () => (
       <h3 className="about-me-title">Kazuhiro Yamada</h3>
       <h4 className="about-me-sub-title">Front-End Engineer / Web-Designer</h4>
       <p className="about-me-text">JavaScriptが好きです。JavaScriptが好きです。</p>
+      <a href="https://github.com/ymdman" target="_blank" rel="noopener noreferrer">
+        <MarkGithubIcon width="32" height="32" />
+      </a>
     </div>
   </div>
 );
