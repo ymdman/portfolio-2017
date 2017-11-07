924bba11fe23eed02233f188bca13b4b8ec45d6f
ymdman <revitalized@icloud.com>
2017-11-07 21:48:39 +0900

    :art: AboutMe アイコン追加

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -9,10 +9,18 @@ const AboutMe = () => (
     <div className="about-me__content">
       <h3 className="about-me-title">Kazuhiro Yamada</h3>
       <h4 className="about-me-sub-title">Front-End Engineer / Web-Designer</h4>
-      <p className="about-me-text">JavaScriptが好きです。JavaScriptが好きです。</p>
-      <a href="https://github.com/ymdman" target="_blank" rel="noopener noreferrer">
-        <MarkGithubIcon width="32" height="32" />
-      </a>
+      <p className="about-me-text">
+        JavaScriptが好きです。JavaScriptが好きです。<br />
+        JavaScriptが好きです。JavaScriptが好きです。
+      </p>
+      <ul className="about-me-social-list">
+        <li>
+          <a href="https://github.com/ymdman" target="_blank" rel="noopener noreferrer">
+            <MarkGithubIcon width="32" height="32" />
+          </a>
+        </li>
+      </ul>
+
     </div>
   </div>
 );
