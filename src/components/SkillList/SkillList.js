213989906fe9f721dcd723562d50da0e76a15d75
ymdman <revitalized@icloud.com>
2017-11-14 23:06:05 +0900

    :art: SkillList 円グラフに変更

--- src/components/SkillList/SkillList.js
+++ src/components/SkillList/SkillList.js
@@ -1,48 +1,25 @@
 import React from 'react';
-import { HorizontalBar } from 'react-chartjs-2';
+import { Doughnut } from 'react-chartjs-2';
 
 const data = {
-  labels: ['JavaScript', 'CSS', 'HTML', 'PHP', 'Photoshop', 'Illustrator'],
   datasets: [{
-    data: [80, 70, 75, 80, 70, 75],
-    label: ['A', 'B', 'C'],
+    data: [80, 20],
+    label: ['A', 'B'],
     backgroundColor: [
       'rgb(255, 99, 132)',
-      'rgb(54, 162, 235)',
-      'rgb(255, 206, 86)',
+      '#fff',
     ],
+    borderWidth: 0,
   },
   ],
 };
 
 const options = {
   responsive: true,
-
+  maintainAspectRatio: false,
+  cutoutPercentage: 80,
   legend: {
     display: false,
-  },
-
-  scales: {
-    yAxes: [{
-      display: true,
-      barPercentage: 0.6,
-      categoryPercentage: 0.6,
-      scaleLabel: {
-        display: true,
-      },
-      gridLines: {
-        display: false,
-      },
-    }],
-
-    xAxes: [{
-      display: true,
-      ticks: {
-        min: 0,
-        max: 100,
-        stepSize: 10,
-      },
-    }],
   },
 
   layout: {
@@ -57,7 +34,7 @@ const options = {
 
 const SkillList = () => (
   <div className="">
-    <HorizontalBar data={data} options={options} />
+    <Doughnut data={data} options={options} width={150} height={150} />
   </div>
 );
 
