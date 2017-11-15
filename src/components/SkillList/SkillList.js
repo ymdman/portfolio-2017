c216ba09e2a94b372f92fd5c06f934e923c31da0
ymdman <revitalized@icloud.com>
2017-11-15 22:55:53 +0900

    :art: ドーナツグラフに変更

--- src/components/SkillList/SkillList.js
+++ src/components/SkillList/SkillList.js
@@ -1,41 +1,64 @@
 import React from 'react';
 import { Doughnut } from 'react-chartjs-2';
 
-const data = {
-  datasets: [{
-    data: [80, 20],
-    label: ['A', 'B'],
-    backgroundColor: [
-      'rgb(255, 99, 132)',
-      '#fff',
-    ],
-    borderWidth: 0,
-  },
+const dataJS = {
+  datasets: [
+    {
+      data: [80, 20],
+      backgroundColor: [
+        '#ffda3f',
+        '#fff',
+      ],
+      borderWidth: 0,
+    },
+  ],
+};
+
+const dataHTML = {
+  datasets: [
+    {
+      data: [80, 20],
+      backgroundColor: [
+        '#e44d27',
+        '#fff',
+      ],
+      borderWidth: 0,
+    },
   ],
 };
 
 const options = {
-  responsive: true,
-  maintainAspectRatio: false,
-  cutoutPercentage: 80,
+  cutoutPercentage: 70,
   legend: {
     display: false,
   },
 
-  layout: {
-    padding: {
-      left: 0,
-      right: 0,
-      top: 0,
-      bottom: 0,
-    },
+  hover: {
+    mode: false,
+  },
+
+  animation: {
+    duration: 2000,
+  },
+
+  tooltips: {
+    enabled: false,
   },
 };
 
 const SkillList = () => (
-  <div className="">
-    <Doughnut data={data} options={options} width={150} height={150} />
-  </div>
+  <ul className="hoge">
+    <li>
+      <h3>JavaScript</h3>
+      <p>80%</p>
+      <Doughnut data={dataJS} options={options} width={150} height={150} />
+    </li>
+    <li>
+      <h3>HTML</h3>
+      <p>80%</p>
+      <Doughnut data={dataHTML} options={options} width={150} height={150} />
+    </li>
+  </ul>
 );
 
 export default SkillList;
