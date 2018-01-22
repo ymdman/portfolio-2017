d952aa1c1c15d604ed15a153df7814d61b5a2647
ymdman <revitalized@icloud.com>
2018-01-22 22:35:37 +0900

    :art: projectレイアウト変更

--- src/components/CareerList/CareerList.js
+++ src/components/CareerList/CareerList.js
@@ -19,8 +19,8 @@ class CareerList extends React.Component {
         {
           this.props.currentState.careerData.map(career => (
             <li key={career.key}>
-              <h3>{career.companyName}</h3>
-              <dl>
+              <h3 className="career-list__title">{career.companyName}</h3>
+              <dl className="career-list__detail">
                 <dt>Job</dt>
                 <dd>{career.job}</dd>
                 <dt>status</dt>
@@ -28,7 +28,7 @@ class CareerList extends React.Component {
                 <dt>experience</dt>
                 <dd>{career.experience}</dd>
               </dl>
-              <p>{career.description}</p>
+              <p className="career-list__description">{career.description}</p>
               <ul className="career-list__project">
                 {
                   career.projects.map(projects => (
@@ -38,8 +38,8 @@ class CareerList extends React.Component {
                         role="button"
                         tabIndex="0"
                       >
-                        <h4>{projects.siteName}</h4>
-                        <div><img src={projects.imagePath} alt={projects.siteName} /></div>
+                        <h4 className="project-title">{projects.siteName}</h4>
+                        <div className="project-image"><img src={projects.imagePath} alt={projects.siteName} /></div>
                       </span>
                       <CareerDetail projects={projects} />
                     </li>
@@ -67,6 +67,7 @@ CareerList.defaultProps = {
 const mapStateToProps = state => (
   { currentState: state.Index }
 );
+
 
 export default connect(
   mapStateToProps,
