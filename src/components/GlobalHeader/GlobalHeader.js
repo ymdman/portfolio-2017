d9d374fb2c692aa5d6db079c4b5babe365f3d48d
ymdman <revitalized@icloud.com>
2018-04-12 22:48:35 +0900

    :art: ロゴ変更

--- src/components/GlobalHeader/GlobalHeader.js
+++ src/components/GlobalHeader/GlobalHeader.js
@@ -3,7 +3,10 @@ import MenuButton from '../MenuButton/MenuButton';
 
 const GlobalHeader = props => (
   <header className="global-header">
-    <h1 className="global-header__logo">19790620.com</h1>
+    <h1 className="global-header-logo">
+      <span className="global-header-logo__main">19790620</span>
+      <span className="global-header-logo__sub">.com</span>
+    </h1>
     <MenuButton {...props} />
   </header>
 );
