f44f11afb4befa6b47b5da2e3da5aecb57565215
ymdman <revitalized@icloud.com>
2018-04-23 23:05:50 +0900

    :zap: モーダル スタイル変更

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -8,11 +8,28 @@ class Modal extends React.Component {
     super();
 
     this.toggleModal = this.toggleModal.bind(this);
+    this.getWindowHeight = this.getWindowHeight.bind(this);
+  }
+
+  componentDidMount() {
+    this.getWindowHeight();
+
+    window.addEventListener(
+      'resize',
+      () => {
+        this.getWindowHeight();
+      },
+      false,
+    );
+  }
+
+  getWindowHeight() {
+    this.props.actions.getWindowHeight();
   }
 
   toggleModal() {
     if (this.props.currentState.showModal) {
-      return { display: 'table' };
+      return { display: 'block' };
     }
 
     return { display: 'none' };
@@ -21,59 +38,71 @@ class Modal extends React.Component {
   render() {
     return (
       <div className="modal" style={this.toggleModal()}>
-        <div className="modal__inner">
-          <h2>{this.props.currentState.modalContent.siteName}</h2>
-          <p>{this.props.currentState.modalContent.description}</p>
-          <ul className="modal-image-list">
-            {this.props.currentState.modalContent.image.map(image => (
-              <li key={image.key}>
-                <img
-                  src={image.url}
-                  alt={image.alt}
-                  className="modal-image-list__image"
-                />
-              </li>
-            ))}
-          </ul>
-          <dl>
-            <dt>Site / サイト</dt>
-            <dd>{this.props.currentState.modalContent.siteName}</dd>
-            <dt>Charge / 担当</dt>
-            <dd>{this.props.currentState.modalContent.charge}</dd>
-            <dt>Experience / 期間</dt>
-            <dd>{this.props.currentState.modalContent.experience}</dd>
-            <dt>Other / その他</dt>
-            <dd>{this.props.currentState.modalContent.other}</dd>
-          </dl>
+        <div
+          className="modal__inner"
+          style={{ height: this.props.currentState.windowHeight - 120 }}
+        >
+          <div className="modal-datail">
+            <h2 className="modal-datail__title">
+              {this.props.currentState.modalContent.siteName}
+            </h2>
+            <p className="modal-datail__description">
+              {this.props.currentState.modalContent.description}
+            </p>
+            <dl className="modal-datail__list">
+              <dt>Site / サイト</dt>
+              <dd>{this.props.currentState.modalContent.siteName}</dd>
+              <dt>Charge / 担当</dt>
+              <dd>{this.props.currentState.modalContent.charge}</dd>
+              <dt>Experience / 期間</dt>
+              <dd>{this.props.currentState.modalContent.experience}</dd>
+              <dt>Other / その他</dt>
+              <dd>{this.props.currentState.modalContent.other}</dd>
+            </dl>
 
-          {(() => {
-            if (!this.props.currentState.modalContent.url) {
-              return false;
-            }
+            {(() => {
+              if (!this.props.currentState.modalContent.url) {
+                return false;
+              }
 
-            return (
-              <p>
-                <a
-                  href={this.props.currentState.modalContent.url}
-                  target="_blank"
-                >
-                  Visit This Site
-                </a>
-              </p>
-            );
-          })()}
-
-          <div
-            onClick={() => {
-              this.props.actions.showModal({ image: [] });
-              this.toggleModal();
-            }}
-            role="button"
-            tabIndex="0"
-            className="modal__close-btn"
-          >
-            close
+              return (
+                <p>
+                  <a
+                    href={this.props.currentState.modalContent.url}
+                    target="_blank"
+                  >
+                    Visit This Site
+                  </a>
+                </p>
+              );
+            })()}
           </div>
+
+          <div className="modal-image">
+            <ul className="modal-image__list">
+              {this.props.currentState.modalContent.image.map(image => (
+                <li key={image.key}>
+                  <img
+                    src={image.url}
+                    alt={image.alt}
+                    className="modal-image-list__image"
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        <div
+          onClick={() => {
+            this.props.actions.showModal({ image: [] });
+            this.toggleModal();
+          }}
+          role="button"
+          tabIndex="0"
+          className="modal__close-btn"
+        >
+          close
         </div>
       </div>
     );
@@ -83,10 +112,12 @@ class Modal extends React.Component {
 Modal.propTypes = {
   actions: React.PropTypes.shape({
     showModal: React.PropTypes.func.isRequired,
+    getWindowHeight: React.PropTypes.func.isRequired,
   }),
   currentState: React.PropTypes.shape({
     modalContent: React.PropTypes.object.isRequired,
     showModal: React.PropTypes.boolean,
+    windowHeight: React.PropTypes.number.isRequired,
   }),
 };
 
