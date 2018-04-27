57b8802f80374ec5713133db144eef245b33f04c
ymdman <revitalized@icloud.com>
2018-04-27 15:45:05 +0900

    :zap: メニューボタン変更 CSS→SVG

--- src/components/GlobalHeader/GlobalHeader.js
+++ src/components/GlobalHeader/GlobalHeader.js
@@ -1,14 +1,33 @@
 import React from 'react';
-import MenuButton from '../MenuButton/MenuButton';
+import { ThreeBarsIcon } from 'react-octicons';
 
 const GlobalHeader = props => (
   <header className="global-header">
-    <h1 className="global-header-logo">
-      <span className="global-header-logo__main">19790620</span>
-      <span className="global-header-logo__sub">.com</span>
+    <h1 className="global-header__logo">
+      <span className="global-header-logo-main">19790620</span>
+      <span className="global-header-logo-sub">.com</span>
     </h1>
-    <MenuButton {...props} />
+    <ThreeBarsIcon
+      onClick={() => {
+        props.actions.changeDrawerMenu();
+      }}
+      tabIndex="0"
+      role="button"
+      width="32"
+      height="32"
+      className="global-haeder__menu-button"
+    />
   </header>
 );
+
+GlobalHeader.propTypes = {
+  actions: React.PropTypes.shape({
+    changeDrawerMenu: React.PropTypes.func.isRequired,
+  }),
+};
+
+GlobalHeader.defaultProps = {
+  actions: {},
+};
 
 export default GlobalHeader;
